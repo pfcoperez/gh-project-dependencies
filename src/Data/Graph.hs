093b964de7ad9738aeddbdc2f36ce2e678059d67
pfcoperez <contact@pablofranciscoperez.info>
2018-11-11 16:57:35 +0100

    Graph now can piggy back a label to node entries.

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -7,39 +7,44 @@ import qualified Data.Set as Set
 import qualified Data.Maybe as Maybe
 import Data.Tree as Tree
 
-type Edges a = Set.Set a
-type Graph a = Map.Map a (Edges a)
+type Edges k = Set.Set k
+type Graph k v = Map.Map k (v, (Edges k))
 
-empty :: Ord a => Graph a
+empty :: Ord k => Graph k v
 empty = Map.empty
 
-addNode :: Ord a => a -> Graph a -> Graph a
-addNode node graph = Map.insertWith (\ new old -> old ) node Set.empty graph
+addNode :: Ord k => (k, v) -> Graph k v -> Graph k v
+addNode (k, v) graph = Map.insertWith (\ new old -> old ) k (v, Set.empty) graph
 
-addEdge :: Ord a => a -> a -> Graph a -> Graph a
-addEdge from to graph = Map.insertWith (\ new old -> Set.union new old) from (Set.singleton to) graph
+addEdge :: Ord k => k -> k -> Graph k v -> Graph k v
+addEdge from to graph = Map.update (\ (oldV, oldEdges) -> Just (oldV, Set.insert to oldEdges)) from graph
 
-nodes :: Graph a -> [a]
+nodes :: Graph k v -> [k]
 nodes = Map.keys
 
-edges :: Ord a => a -> Graph a -> [a]
-edges from graph = (Maybe.maybeToList (graph Map.!? from)) >>= Set.toList
+edges :: Ord k => k -> Graph k v -> [k]
+edges from graph = (Maybe.maybeToList (graph Map.!? from)) >>= (Set.toList . snd)
 
-deepExplore :: Ord a => (a -> [a]) -> [a] -> Graph a -> Graph a
-deepExplore _ [] acc = acc
-deepExplore discover (current:rem) acc =
+nodeValue :: Ord k => k -> Graph k v -> v
+nodeValue node graph = let (v, _) = (graph Map.! node) in v
+
+deepExplore :: Ord k => (k -> [k]) -> (k -> v) -> [k] -> Graph k v -> Graph k v
+deepExplore _ _ [] acc = acc
+deepExplore discover details (current:rem) acc =
   let adjacent = discover current
-      updatedGraph = addNode current $ foldl ( \ graph node -> (addEdge current node) graph ) acc adjacent
-  in deepExplore discover (filter (\candidate -> not $ Map.member candidate updatedGraph) $ adjacent ++ rem) updatedGraph
+      value = details current
+      updatedGraph = addNode (current, value) $ foldl ( \ graph node -> (addEdge current node) graph ) acc adjacent
+  in deepExplore discover details (filter (\candidate -> not $ Map.member candidate updatedGraph) $ adjacent ++ rem) updatedGraph
 
-deepExploreIO :: Ord a => (a -> IO [a]) -> [a] -> Graph a -> IO (Graph a)
-deepExploreIO _ [] acc = return acc
-deepExploreIO discover (current:rem) acc = do
+deepExploreIO :: (Ord k, Monad m) => (k -> m [k]) -> (k -> m v) -> [k] -> Graph k v -> m (Graph k v)
+deepExploreIO _ _ [] acc = return acc
+deepExploreIO discover details (current:rem) acc = do
   adjacent <- discover current
-  let updatedGraph = addNode current $ foldl ( \ graph node -> (addEdge current node) graph ) acc adjacent
-  deepExploreIO discover (filter (\candidate -> not $ Map.member candidate updatedGraph) $ adjacent ++ rem) updatedGraph
+  value <- details current
+  let updatedGraph = addNode (current, value) $ foldl ( \ graph node -> (addEdge current node) graph ) acc adjacent
+  deepExploreIO discover details (filter (\candidate -> not $ Map.member candidate updatedGraph) $ adjacent ++ rem) updatedGraph
 
-graphToTree :: Ord a => a -> Set.Set a -> Graph a -> Tree a
+graphToTree :: Ord k => k -> Set.Set k -> Graph k v -> Tree v
 graphToTree node visited graph
-  | Set.member node visited = Tree.Node node []
-  | otherwise = Tree.Node node $ fmap (\child -> graphToTree child (Set.insert node visited) graph) $ edges node graph
+  | Set.member node visited = Tree.Node (nodeValue node graph) []
+  | otherwise = Tree.Node (nodeValue node graph) $ fmap (\child -> graphToTree child (Set.insert node visited) graph) $ edges node graph
